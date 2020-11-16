3cbdd7f7a3a3ab7d796c00eaa4a1a89d52c0e07b
takumi-murakami <takumi28280503@gmail.com>
2020-11-16 12:46:54 +0900

    cash rm

--- app/controllers/report_controller.rb
+++
@@ -1,9 +0,0 @@
-class ReportController < ApplicationController
-  def index
-  end
-
-  def output
-    # @report = Report.new(params[:year].to_i, params[:month].to_i)
-  end
-
-end
--- app/models/comment.rb
+++
@@ -1,7 +0,0 @@
-class Comment < ApplicationRecord
-  belongs_to :user
-  belongs_to :event
-  has_many :notifications, dependent: :destroy
-
-  validates :text, presence: true
-end
